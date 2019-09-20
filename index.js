71c6bd22387b3caf2dfdeaf3b6a55d7f6712a61a
John Azzaro <jazzaro@temple.edu>
2019-09-19 17:38:44 -0700

    preventDefault and messages array

--- index.js
+++ index.js
@@ -1,5 +1,10 @@
 "use strict";
 
-const name = document.getElementById('name');
-const password = document.getElementById('password');
-const form = docuemnt.getElementById('form');
+const name = document.getElementById('name');                       // select "name" element
+const password = document.getElementById('password');               // select "password" element
+const form = docuemnt.getElementById('form');                       // select "form" element
+
+form.addEventListener('submit', function(event) {                        
+    let messages = [];                                              // "messages" storing all the error messages for the form validation.
+    event.preventDefault()                                          // prevent page from submitting
+});
